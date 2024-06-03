67ed01a241541169baa7118fbd909aed7634248f
root <root@debian>
2024-06-03 20:49:27 +0200

    fix uncommented comment

--- core/jdefl.h
+++ core/jdefl.h
@@ -70,7 +70,7 @@
 // AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
   
 #include <string.h>
 
