8b1d2d755085f4cae8978aed06c78ea996f44180
root <root@debian>
2024-06-03 20:53:36 +0200

    build everthing

--- tools/build_gcc.sh
+++ tools/build_gcc.sh
@@ -5,7 +5,7 @@ FLAGS="-Wextra -Wall -ggdb"
 # FLAGS=
 
 gcc $FLAGS -o bin/http_server src/http_server.c
-# gcc $FLAGS -o bin/color_picker src/color_picker.c -lGLX -lX11 -lGL -lm
-# gcc $FLAGS -o bin/music_player src/music_player.c -lasound
-# gcc $FLAGS -o bin/image_converter src/image_converter.c -lm
-# gcc $FLAGS -o bin/image_viewer src/image_viewer.c -lGLX -lX11 -lGL -lm
+gcc $FLAGS -o bin/color_picker src/color_picker.c -lGLX -lX11 -lGL -lm
+gcc $FLAGS -o bin/music_player src/music_player.c -lasound
+gcc $FLAGS -o bin/image_converter src/image_converter.c -lm
+gcc $FLAGS -o bin/image_viewer src/image_viewer.c -lGLX -lX11 -lGL -lm
